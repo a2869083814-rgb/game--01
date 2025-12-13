5d78ace36fb2057cf64cc15066900cd4843cbf17
a2869083814-rgb <a2869083814@gmail.com>
2025-12-13 12:01:23 +0800

    game Projet
    first try

---
+++ Assets/脚本/控制.cs
@@ -0,0 +1,41 @@
+
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class 控制 : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float rotateSpeed = 10f;
+    Animator anim;
+    Vector3 direction;//传递给动画的moveSpeed
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        direction = new Vector3(h, 0, v);
+
+        if (direction.magnitude > 0.1f)
+        {
+            transform.Translate(direction * moveSpeed * Time.deltaTime, Space.World);
+
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+        }
+        UpdateAnim();
+    }
+    void UpdateAnim()
+    {
+        if (anim != null)
+        {
+            anim.SetFloat("moveSpeed", direction.magnitude);
+        }
+    }
+}
